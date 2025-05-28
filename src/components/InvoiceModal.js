2567aeefea5a7354f5a18829e4dd6815f3fa728c
anshitashukla <anshita.shukla@gwu.edu>
2025-05-28 13:32:09 -0400

    modified new invoice

--- src/components/InvoiceModal.js
+++ src/components/InvoiceModal.js
@@ -26,6 +26,28 @@ function GenerateInvoice() {
   });
 }
 
+function SendInvoice() {
+  html2canvas(document.querySelector("#invoiceCapture")).then((canvas) => {
+    const imgData = canvas.toDataURL("image/png", 1.0);
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "pt",
+      format: [612, 792],
+    });
+    pdf.internal.scaleFactor = 1;
+    const imgProps = pdf.getImageProperties(imgData);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+
+    // Save the PDF
+    pdf.save("invoice-001.pdf");
+
+    // Open default email client with blank mailto
+    window.location.href = "mailto:";
+  });
+}
+
 class InvoiceModal extends React.Component {
   constructor(props) {
     super(props);
@@ -50,9 +72,8 @@ class InvoiceModal extends React.Component {
                 </h6>
               </div>
               <div className="text-end ms-4">
-                <h6 className="fw-bold mt-1 mb-2">Amount&nbsp;Due:</h6>
+                <h6 className="fw-bold mt-1 mb-2">Amount Due:</h6>
                 <h5 className="fw-bold">
-                  {" "}
                   {this.props.currency} {this.props.total}
                 </h5>
               </div>
@@ -166,7 +187,7 @@ class InvoiceModal extends React.Component {
                 <Button
                   variant="primary"
                   className="d-block w-100"
-                  onClick={GenerateInvoice}
+                  onClick={SendInvoice}
                 >
                   <BiPaperPlane
                     style={{ width: "15px", height: "15px", marginTop: "-3px" }}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,20 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
+  const handleNewInvoice = () => {
+    window.location.reload();
+  };
+
   return (
     <header className="header">
-      <div className="container-fluid">
-        <h1 className="header-title text-start">Easy Bill</h1>
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <h1 className="header-title">Easy Bill</h1>
+        <button
+          className="btn btn-light new-invoice-btn"
+          onClick={handleNewInvoice}
+        >
+          New Invoice
+        </button>
       </div>
     </header>
   );
